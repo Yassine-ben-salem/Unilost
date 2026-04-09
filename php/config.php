f898163de54d0c02f91ef04f8eb70a136023396e
Med Yassine Bensalem <medyassinebnsalem@gmail.com>
2026-04-09 15:41:02 +0100

    Cast MYSQLPORT to int for database connection

--- php/config.php
+++ php/config.php
@@ -3,9 +3,9 @@ $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
-$port = getenv('MYSQLPORT');
+$port = (int)getenv('MYSQLPORT');
 
-$conn = mysqli_connect($host, $user, $pass, $db, (int)$port);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
